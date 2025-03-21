eca3dadb59f48663b1ad313baf2df8708d550104
Даня Сафин <danyasafin@icloud.com>
2025-03-21 15:16:37 +0300

    add Body component, new index page, and update Nuxt config for fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/fonts"],
   css: ["~/assets/css/tailwind.css"],
   vite: {
     plugins: [tailwindcss()],
+  },
+  fonts: {
+    defaults: {
+      weights: [300, 700, 900],
+      styles: ["normal"],
+      subsets: ["cyrillic", "latin"],
+    },
   },
 });
