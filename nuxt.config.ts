292776e88d6695338e345ebc9827568f0a031769
Даня Сафин <danyasafin@icloud.com>
2025-03-21 13:55:57 +0300

    add tw, prettier, eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
-})
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
