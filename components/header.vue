e51e8fe47a1f77de8563a05d6d76470629887281
Даня Сафин <danyasafin@icloud.com>
2025-03-24 01:09:46 +0300

    enhance header component with responsive drawer and layout adjustments

--- components/header.vue
+++ components/header.vue
@@ -10,13 +10,13 @@ const links = [
 
 <template>
   <nav
-    class="sticky top-0 flex flex-row justify-center gap-10 border-b border-b-blue-100 bg-white px-10 py-3"
+    class="sticky top-0 flex flex-row justify-between gap-10 border-b border-b-blue-100 bg-white px-5 py-3 sm:justify-center sm:px-10"
   >
     <div class="flex items-center justify-center gap-3">
       <img src="/icon.png" alt="icon" class="size-10" />
       <span class="text-lg font-bold">metodichka</span>
     </div>
-    <div class="flex flex-1 flex-row justify-center gap-2">
+    <div class="hidden flex-1 flex-row justify-center gap-2 sm:flex">
       <UButton
         :to="link.to"
         variant="link"
@@ -27,5 +27,23 @@ const links = [
         {{ link.title }}
       </UButton>
     </div>
+    <UDrawer direction="right">
+      <UButton variant="ghost" color="neutral">
+        <UIcon name="i-lucide-align-justify" class="size-5" />
+      </UButton>
+      <template #body>
+        <div class="flex flex-col">
+          <UButton
+            :to="link.to"
+            variant="link"
+            v-for="link in links"
+            class="text-lg text-black hover:text-blue-500"
+            active-class="bg-blue-100"
+          >
+            {{ link.title }}
+          </UButton>
+        </div>
+      </template>
+    </UDrawer>
   </nav>
 </template>
