741638d6273fb96dcb20fa94f015f2c5640c81e2
Даня Сафин <danyasafin@icloud.com>
2025-03-24 09:36:00 +0300

    add @nuxtjs/mdc module and update index.vue with new layout and styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/fonts"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/fonts", "@nuxtjs/mdc"],
   css: ["~/assets/css/tailwind.css"],
   vite: {
     plugins: [tailwindcss()],
--- pages/help/index.vue
+++ pages/help/index.vue
@@ -1,3 +1,46 @@
+<script setup lang="ts">
+const items = [{ label: "app", children: [{ label: "app", children: [] }] }];
+const page = "# Hello";
+</script>
+
 <template>
-  <div></div>
+  <main class="container mx-auto my-10 flex gap-2 px-5">
+    <UTree :items="items" color="info" />
+    <MDC class="reset-tw" :value="page" />
+  </main>
 </template>
+
+<style lang="scss">
+.reset-tw {
+  h1 {
+    font-size: var(--text-xl);
+    line-height: var(--tw-leading, var(--text-xl--line-height));
+    font-weight: var(--font-weight-black);
+  }
+  h2 {
+    font-size: var(--text-lg);
+    line-height: var(--tw-leading, var(--text-lg--line-height));
+    font-weight: var(--font-weight-extrabold);
+  }
+  h3 {
+    font-size: var(--text-base);
+    line-height: var(--tw-leading, var(--text-base--line-height));
+    font-weight: var(--font-weight-extrabold);
+  }
+  h4 {
+    font-size: var(--text-base);
+    line-height: var(--tw-leading, var(--text-base--line-height));
+    font-weight: var(--font-weight-bold);
+  }
+  h5 {
+    font-size: var(--text-sm);
+    line-height: var(--tw-leading, var(--text-sm--line-height));
+    font-weight: var(--font-weight-bold);
+  }
+  h6 {
+    font-size: var(--text-xs);
+    line-height: var(--tw-leading, var(--text-xs--line-height));
+    font-weight: var(--font-weight-bold);
+  }
+}
+</style>
