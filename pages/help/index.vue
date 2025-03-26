ea36aae11ec699b52836a5c2b5078514578881ab
Даня Сафин <danyasafin@icloud.com>
2025-03-26 11:05:31 +0300

    add index.vue with initial setup, computed properties, and updated template structure for improved functionality

--- pages/help/index.vue
+++ pages/help/index.vue
@@ -29,12 +29,20 @@ const formatted = computed(() =>
     onSelect() {
       router.push({ query: { ...route.query, i: v.id } });
     },
+    onToggle() {
+      if (route.query.i === String(v.id)) {
+        router.push({ query: { ...route.query, i: 1 } });
+      }
+    },
   })),
 );
 
-const selected = computed(() =>
-  formatted.value?.find((v) => v.value === route.query.i),
-);
+const selected = computed({
+  get() {
+    return formatted.value?.find((v) => v.value === route.query.i);
+  },
+  set(_) {},
+});
 
 const struct = buildTree(formatted.value ?? []);
 
@@ -58,7 +66,7 @@ function findSelected(categories: (Category & TreeItem)[]): boolean {
 findSelected(struct);
 
 const v = computed(() =>
-  useFetch<Page>(`${config.public.apiUrl}/pages/${route.query.i}`),
+  useFetch<Page>(`${config.public.apiUrl}/pages/${route.query.i ?? 0}`),
 );
 const markdown = computed(() => v.value.data.value?.markdown);
 const lastMarkdown = ref(markdown.value);
@@ -69,16 +77,18 @@ watchEffect(() => {
 
 <template>
   <main class="container mx-auto my-10 flex flex-col gap-2 px-5 md:flex-row">
-    <UTree
-      class="w-full md:w-1/4"
-      :items="struct"
-      :default-value="selected"
-      selection-behavior="replace"
-      :propagate-select="false"
-      trailing-icon="i-lucide-chevron-down"
-      expanded-icon="i-lucide-book-open"
-      collapsed-icon="i-lucide-book"
-    />
+    <div class="flex w-full flex-col md:w-1/4">
+      <UInput />
+      <UTree
+        :items="struct"
+        v-model="selected"
+        selection-behavior="replace"
+        :propagate-select="false"
+        trailing-icon="i-lucide-square-chevron-down"
+        expanded-icon="i-lucide-book-open"
+        collapsed-icon="i-lucide-book"
+      />
+    </div>
     <MDC class="reset-tw w-full md:w-3/4" :value="lastMarkdown ?? ''" />
   </main>
 </template>
@@ -114,6 +124,30 @@ watchEffect(() => {
     font-size: var(--text-xs);
     line-height: var(--tw-leading, var(--text-xs--line-height));
     font-weight: var(--font-weight-bold);
+  }
+  blockquote {
+    background: var(--color-blue-50);
+  }
+  th,
+  td {
+    padding: calc(var(--spacing) * 2);
+    border: 1px solid var(--color-blue-200);
+  }
+  ol {
+    list-style-type: decimal;
+  }
+  ul {
+    list-style-type: disc;
+  }
+  ul,
+  ol {
+    margin-left: calc(var(--spacing) * 6);
+  }
+  p {
+    margin: calc(var(--spacing) * 4) 0;
+  }
+  a {
+    text-decoration: underline;
   }
 }
 </style>
